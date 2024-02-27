8cb93e18fd74d2eab733c4080511126304446a34
Diego dev <diego.oliveiraamaro@gmail.com>
2024-02-27 13:58:15 -0300

    OK

---
+++ infra/secrets.js
@@ -0,0 +1,20 @@
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
+
+async function accessSecret() {
+    const client = new SecretManagerServiceClient();
+
+    const secretName = 'projects/35220706239/secrets/Teste_Acesso_DB/versions/1';
+
+    const [version] = await client.accessSecretVersion({
+        name: secretName,
+    });
+
+    const payload = version.payload.data.toString('utf8');
+
+    console.log(`Payload: ${payload}`);
+}
+
+accessSecret().catch('teste erro secret');
+
+
+
